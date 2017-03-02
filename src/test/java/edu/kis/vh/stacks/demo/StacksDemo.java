f956cfab8459c287de57c8bc6e78b7c673772cf9
Maciej Kacprzak <194971@edu.p.lodz.pl>
2017-03-02 15:04:32 +0100

    Poprawiono wciecia w klasach stosu. Dodano komentarze.

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -4,6 +4,19 @@ import edu.kis.vh.stacks.StackHanoi;
 import edu.kis.vh.stacks.stack;
 import edu.kis.vh.stacks.factory.DefaultStacksFactory;
 
+// Poprawiłem wcięcia metod w klasie "stack"
+// Wiersze: 14-32
+// Poprawiłem wcięcie linijki pod if'em w klasie "stack"
+// Wiersz: 11
+
+// Poprawiłem wcięcia linijek pod while'em w klasie "StackFIFO"
+// Wiersze: 10, 15
+
+// Poprawiłem wcięcie linijek w klasie "StackHanoi"
+// Wiersze: 5,10,12-15
+
+// Kombinacja Alt + -> i Alt + <- umożliwa nawigację po miejscach w plikach, w których ostatnio byłem
+
 class StacksDemo {
 
 	public static void main(String[] args) {
