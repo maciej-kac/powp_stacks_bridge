ba04f3a1f2304938133e7d28e6567b62b3e669ec
Maciej Kacprzak <194971@edu.p.lodz.pl>
2017-03-29 22:36:03 +0200

    Zmieniono wartosc zwracana w przypadku pustego stosu.

--- src/main/java/edu/kis/vh/stacks/IStack.java
+++ src/main/java/edu/kis/vh/stacks/IStack.java
@@ -2,7 +2,7 @@ package edu.kis.vh.stacks;
 
 public interface IStack {
 
-	public int EMPTY_STACK_INDICATOR = -1;
+	public int EMPTY_STACK_INDICATOR = 0;
 	public int MAX_ITEMS = 12;
 
 	int getTotal();
@@ -17,4 +17,6 @@ public interface IStack {
 
 	int pop();
 
+	//Oczywiście realizacja zadania 10 pomogła mi w zmianie wartości stałej zwracanej gdy stos jest pusty.
+	//Wykonanie zadanie wymagało ode mnie zmiany wartości tylko w jednym miejscu, a nie w sześciu.
 }
